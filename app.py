3e0afeb2ea0fc707f7c3fd777faba3c13fa1172d
varunpurohit76 <varunpurohit76@gmail.com>
2016-11-28 22:32:10 +0530

    app domains

--- app.py
+++ app.py
@@ -20,7 +20,60 @@ def page_not_found(e):
 def index():
     return render_template('index.html')
 
+#----------------------------------------
+# facebook authentication
+#----------------------------------------
+
+from flask import url_for, request, session, redirect
+from flask_oauth import OAuth
+
+FACEBOOK_APP_ID = '303096310090447'
+FACEBOOK_APP_SECRET = 'a588d4d7f0c226bcfe98b22d7a1cff4a'
+
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('email, ')}
+)
+
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_token')
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('facebook_token', None)
+
+@app.route("/facebook_login")
+def facebook_login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next'), _external=True))
+
+@app.route("/facebook_authorized")
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None or 'access_token' not in resp:
+        return redirect(next_url)
+
+    session['logged_in'] = True
+    session['facebook_token'] = (resp['access_token'], '')
+
+    return redirect(next_url)
+
+@app.route("/logout")
+def logout():
+    pop_login_session()
+    return redirect(url_for('index'))
+
 # launch
 if __name__ == "__main__":
+    app.config["SECRET_KEY"] = 'a588d4d7f0c226bcfe98b22d7a1cff4a'
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
